66ddc22d2be545fc6c245555ae6163633eff4536
Amarnath Sakthivel <63423850+Amarnath-Sakthivel@users.noreply.github.com>
2020-05-14 10:53:39 -0400

    initial

--- CountChar.java
+++ CountChar.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 
 public class CountChar {
     static String CountChar(String input1) {
-          char first=input1.charAt(0);
-          char last=input1.charAt(input1.length()-1);
-          int len=(input1.substring(1,input1.length()-1)).length();
-          System.out.println(first+""+len+""+last);
+          if(input1.length()>10) {
+              char first = input1.charAt(0);
+              char last = input1.charAt(input1.length() - 1);
+              int len = (input1.substring(1, input1.length() - 1)).length();
+              System.out.println(first + "" + len + "" + last);
+          }
+          else{
+              System.out.println(input1);
+          }
 
 
         return null;
