0cd4c144b52b287356d7d268a1c1d343b16f3953
Amarnath Sakthivel <63423850+Amarnath-Sakthivel@users.noreply.github.com>
2020-05-15 01:15:54 -0400

    Update CountChar.java

--- CountChar.java
+++ CountChar.java
@@ -6,13 +6,9 @@ public class CountChar {
           char last=input1.charAt(input1.length()-1);
           int len=(input1.substring(1,input1.length()-1)).length();
           System.out.println(first+""+len+""+last);
-
-
-        return null;
+          return null;
     }
-
-
-    public static void main(String args[]) {
+        public static void main(String args[]) {
         Scanner sc=new Scanner(System.in);
         String input1=sc.nextLine();
         CountChar(input1);
